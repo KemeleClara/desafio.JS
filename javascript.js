9e18b9d1cd02463307ffc5d540f2e147b7990e3a
Kêmele Clara Alves Mendonça <116192157+KemeleClara@users.noreply.github.com>
2023-05-23 16:14:50 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+ 
+function ativarNome(){
+    let nome = 'Sávio';
+    console.log(nome)
+    document.getElementById('aparecer').innerHTML = nome
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a+b;
+    console.log(a,b,c)
+document.getElementById('aparecer').innerHTML = c
+
+}
